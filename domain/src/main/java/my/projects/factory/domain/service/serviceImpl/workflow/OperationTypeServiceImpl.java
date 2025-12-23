c1679c19fa7a4a9124528297662040c48726df5e
DanielBenda <dan.benda@gmail.com>
2025-12-23 21:10:27 +0100

    OperationType entity introduces, enhanced database build

---
+++ domain/src/main/java/my/projects/factory/domain/service/serviceImpl/workflow/OperationTypeServiceImpl.java
@@ -0,0 +1,26 @@
+package my.projects.factory.domain.service.serviceImpl.workflow;
+
+import my.projects.factory.domain.mapper.workflow.OperationTypeMapper;
+import my.projects.factory.domain.model.workflow.OperationTypeModel;
+import my.projects.factory.domain.service.serviceImpl.CrudServiceImpl;
+import my.projects.factory.domain.service.workflow.OperationTypeService;
+import my.projects.factory.persistence.entity.workflow.OperationType;
+import my.projects.factory.persistence.repository.workflow.OperationTypeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+/**
+ * Implementation of {@link OperationTypeService} that handles business logic
+ * for OperationType entities. Uses {@link OperationTypeMapper} to convert between
+ * {@link OperationType} entities and {@link OperationTypeModel} domain models.
+ */
+@Service
+public class OperationTypeServiceImpl
+        extends CrudServiceImpl<OperationTypeModel, OperationType, UUID>
+        implements OperationTypeService {
+
+    protected OperationTypeServiceImpl(OperationTypeRepository repository, OperationTypeMapper mapper) {
+        super(repository, mapper::toModel, mapper::toEntity);
+    }
+}
---
+++ persistence/src/main/java/my/projects/factory/persistence/schema/workflow/OperationTypeSchema.java
@@ -0,0 +1,19 @@
+package my.projects.factory.persistence.schema.workflow;
+
+/**
+ * Database schema constants for the {@code t_operation_type} table.
+ * <p>
+ * Contains table name, schema name, and column names for the OperationType entity.
+ */
+public class OperationTypeSchema {
+
+    public static final String TABLE = "t_operation_type";
+    public static final String SCHEMA = "factory";
+    public static final String ID = "id";
+    public static final String DESCRIPTION = "description";
+    public static final String NAME = "name";
+    public static final String CREATED = "created";
+    public static final String CREATED_BY = "created_by";
+
+    private OperationTypeSchema() {}
+}
